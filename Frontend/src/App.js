1eae700c95472a3eba88256b82524c0032e1866a
nassilio <hubmattfac@fac.com>
2020-01-29 15:42:42 +0100

    modified app for Main component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./reset.css";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }

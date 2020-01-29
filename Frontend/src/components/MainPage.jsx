db56777fd23a56dc3cd591b1c491d3d3d0298cae
nassilio <hubmattfac@fac.com>
2020-01-29 17:03:28 +0100

    modified mainpage for backend

---
+++ Frontend/src/components/MainPage.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Image, CloudinaryContext } from "cloudinary-react";
+
+export default function MainPage() {
+  const [latestPost, setLatestPost] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${backend}/api/main`)
+      .then(({ data }) => {
+        getTeams(data);
+      })
+      .catch(err => {});
+  }, []);
+  return (
+    <div className="mainPage">
+      <div className="latestPost">
+        <div className="subject">{latestPost.subject}</div>
+        <div className="image">
+          <CloudinaryContext cloudName="nas">
+            <Image publicId={latestPost.image} alt="No image" />
+          </CloudinaryContext>
+        </div>
+        <div className="text">
+          <p>{latestPost.text}</p>
+        </div>
+      </div>
+    </div>
+  );
+}

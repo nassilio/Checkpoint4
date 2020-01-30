1a3d229a082578629bc2f8a7cc99e7a62e48161c
nassilio <hubmattfac@fac.com>
2020-01-30 09:45:48 +0100

    modified directory name

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const {
+  CONFIG: { backendPort },
+  db,
+  cloudinary
+} = require("./conf");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(cors());
+
+//Main GET last post
+app.get("/api/main", (req, res) => {
+  const lastPost = req.params.id;
+  db.query(
+    "SELECT subject, text, image FROM post ORDER BY id DESC LIMIT 1",
+    [lastPost],
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+app.listen(backendPort, err => {
+  if (err) {
+    throw new Error("Something bad happened...");
+  }
+});

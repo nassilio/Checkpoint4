d27b23fa2e4dcdb4504d9f9e2fcb1c0903d11ff2
nassilio <hubmattfac@fac.com>
2020-01-30 14:46:32 +0100

    corrected axios for search

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -5,6 +5,8 @@ import { Image, CloudinaryContext } from "cloudinary-react";
 
 export default function MainPage() {
   const [latestPost, setLatestPost] = useState({});
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     axios
       .get(`${backend}/api/main`)
@@ -13,6 +15,15 @@ export default function MainPage() {
       })
       .catch(err => {});
   }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${backend}/api/search/post?subject=${search}`)
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(err => {});
+  }, [search]);
 
   return (
     <div className="mainPage">
@@ -32,6 +43,14 @@ export default function MainPage() {
       ) : (
         <img src="/nothing-Text.jpg" alt="nothing" className="nothingImg" />
       )}
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Cherche des posts selon leur sujet"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        ></input>
+      </div>
     </div>
   );
 }

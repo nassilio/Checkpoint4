21a7e7e13346e3400dbb2cb8c9ffed7d41f489ea
nassilio <hubmattfac@fac.com>
2020-01-30 10:20:15 +0100

    deleted superfluous directory

--- Frontend/src/conf.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-const backend = process.env.REACT_APP_BACKEND;
-const twitch_Client_ID = process.env.REACT_APP_TWITCH_CLIENT_ID;
-
-export { backend, twitch_Client_ID };
-

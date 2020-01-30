58f5326333d10f5d6d9aecc1f910bbe3530e85e1
nassilio <hubmattfac@fac.com>
2020-01-30 11:24:31 +0100

    created routes in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./reset.css";
 import MainPage from "./components/MainPage";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <MainPage />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="search" component={Search} />
+      </Switch>
     </div>
   );
 }

5fbb6c01695df088bb257574ddda3804b731f5ac
nassilio <hubmattfac@fac.com>
2020-02-03 10:51:05 +0100

    added typing areas and button for image upload

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { backend } from "../conf.js";
 
 export default function Post() {
   const [text, setText] = useState("");
   const [subject, setSubject] = useState("");
+  const [file, setFile] = useState(null);
 
   const submit = e => {
     e.preventDefault();
@@ -20,5 +23,35 @@ export default function Post() {
     }
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <form onSubmit={e => submit(e)}>
+        <textarea
+          type="text"
+          name="subject"
+          placeholder="subject..."
+          value={subject}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+          className="headPost"
+          maxLength="500"
+        />
+        <input type="file" />
+        <textarea
+          type="text"
+          name="text"
+          placeholder="Text..."
+          value={text}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+          className="headPost"
+          maxLength="500"
+        />
+
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
 }
